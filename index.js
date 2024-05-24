9ae2d0677172f57b1ba6b10c7de8ab07f93dc100
saidul24 <jihadh32978@gmail.com>
2024-05-25 02:43:01 +0600

    added verify addmin api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express();
 const cors = require("cors")
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
 
 
 // middleWare
@@ -21,6 +21,7 @@ app.use(express.json())
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujjqksd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +35,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client rsto the server	(optional starting in v4.7)
         // await client.connect();
 
         const userCollection = client.db("bistroDb").collection("users");
@@ -42,10 +43,71 @@ async function run() {
         const reviewsCollection = client.db("bistroDb").collection("reviews");
         const cartsCollection = client.db("bistroDb").collection("carts");
 
+
+        // oun middleWare
+        const verifyToken = async (req, res, next) => {
+            const token = req.headers.authorization.split(' ')[1];
+
+            if (!token) {
+                return res.status(401).send({ message: "unauthorized access" })
+            }
+            console.log(token);
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    console.log("err");
+                    return res
+                        .status(401)
+                        .send({ message: "unauthorized access" })
+                }
+                console.log("dec");
+                req.decoded = decoded
+                next()
+            })
+        }
+
+
+
+        // use verify admin after verify token
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: "forbidden access" })
+            }
+            next()
+        }
+
+        // jwt related api 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
+
+
         // users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req?.decoded?.email) {
+                return res
+                    .status(403)
+                    .send({ message: "forbidden access" })
+            }
+            const query = { email: req.decoded.email }
+            const user = await userCollection.findOne(query)
+
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
         })
 
         app.post('/users', async (req, res) => {
@@ -62,8 +124,13 @@ async function run() {
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id
-            const filter = { _id: ObjectId(id) }
-            const result = await userCollection.updateOne(filter)
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
             res.send(result)
         })
 
